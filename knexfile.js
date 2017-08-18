d463b4139f88181b7d4eed790077d477414a9f61
Daniel Bucket <danieljbucket@gmail.com>
2017-08-18 10:28:08 -0600

    setting up heroku

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,13 @@ module.exports = {
       directory: './db/migrations'
     }
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  },
   useNullAsDefault: true
 };
