c9590db4c1d1c4d7aefcac3bc881ebfbd069d0de
Daniel Bucket <danieljbucket@gmail.com>
2017-08-18 11:49:04 -0600

    update test path

--- test/serverTest.spec.js
+++ test/serverTest.spec.js
@@ -1,12 +1,12 @@
 const chai = require('chai')
 const should = chai.should()
 const chaiHTTP = require('chai-http')
-const server = require('./server')
+const server = require('../server/server')
 
 chai.use(chaiHTTP)
 
 describe('', () => {
   it('', () => {
-    
+
   })
 })
