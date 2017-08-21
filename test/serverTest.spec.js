688d0713243a908978b8cedcd5b42cde13fee108
Daniel Bucket <danieljbucket@gmail.com>
2017-08-21 10:16:16 -0600

    git hooks

--- test/serverTest.spec.js
+++ test/serverTest.spec.js
@@ -1,7 +1,7 @@
 const chai      = require('chai')
 const should    = chai.should()
 const chaiHTTP  = require('chai-http')
-const serveer    = require('../server/server')
+const server    = require('../server/server')
 
 chai.use(chaiHTTP)
 
