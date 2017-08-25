da16b287eca9e6655ab4b3fe0adbffb112fa9ba5
Daniel Bucket <danieljbucket@gmail.com>
2017-08-24 19:16:50 -0600

    newmessage

--- test/serverTest.spec.js
+++ test/serverTest.spec.js
@@ -1,7 +1,12 @@
+process.env.NODE_ENV = 'test';
 const chai      = require('chai')
 const should    = chai.should()
 const chaiHTTP  = require('chai-http')
-const server    = require('../server/server')
+const server    = require('../server/server');
+
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('../knexfile')[environment]
+const db = require('knex')(configuration)
 
 chai.use(chaiHTTP)
 
